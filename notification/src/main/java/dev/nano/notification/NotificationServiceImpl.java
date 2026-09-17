6b4fc5ddaa67b65101fa8af4019fe658497d0895
ravi <ravikirank@tblocks.com>
2026-09-17 16:16:06 +0530

    Add notification service to GKE and remove AWS

--- notification/src/main/java/dev/nano/notification/NotificationServiceImpl.java
+++ notification/src/main/java/dev/nano/notification/NotificationServiceImpl.java
@@ -1,7 +1,6 @@
 package dev.nano.notification;
 
 import dev.nano.clients.notification.NotificationRequest;
-import dev.nano.notification.aws.AWSEmailService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +13,6 @@ public class NotificationServiceImpl implements NotificationService {
 
     private final NotificationRepository notificationRepository;
     private final NotificationMapper notificationMapper;
-    private final AWSEmailService emailService;
 
     @Override
     public NotificationDTO getNotification(Long notificationId) {
@@ -42,19 +40,5 @@ public class NotificationServiceImpl implements NotificationService {
                 .message(notificationRequest.getMessage())
                 .sentAt(LocalDateTime.now())
                 .build());
-
-        emailService.send(
-                notificationRequest.getCustomerEmail(),
-                buildEmail(notificationRequest.getCustomerName(), notificationRequest.getMessage()),
-                notificationRequest.getSender()
-        );
-    }
-
-    public String buildEmail(String name, String message) {
-        return """
-                Mail sent to: ${name}
-                <p>${message}</p>
-        """;
-
     }
 }
